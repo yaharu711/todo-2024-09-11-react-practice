43e44d726a8bb2724cc1e0d4acdebd0fa7395baa
yaharu711 <yah7yah707@gmail.com>
2024-09-13 09:35:28 +0900

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/todo-2024-09-11-react/' : '/',
   server: {
     host: '0.0.0.0',
     port: 5173, // 必要に応じてポート番号を指定
